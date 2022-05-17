cabb9ad05ec2c9bf07e1f3ad66831797370b09ba
Aldi Wiranata <aldiw01@gmail.com>
2022-05-18 05:33:46 +0700

    Fix watermark issues

--- src/routes/uploads.js
+++ src/routes/uploads.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
 const path = require('path')
+const { degrees, PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 const fs = require('fs');
-const { PDFNet } = require('@pdftron/pdfnet-node');
 const moment = require('moment')
 
 /////////////////////////////////////////////////////////////////////////////////////////////
@@ -10,9 +10,8 @@ const moment = require('moment')
 
 router.use('/', express.static(path.join(__dirname, '../uploads')))
 
-router.post('/watermark/:filename', (req, res) => {
+router.post('/watermark/:filename', async (req, res) => {
   const filename = req.params.filename;
-  console.log(req.body)
   const status = req.body.status === '1' ? 'ACTIVE' : 'OBSOLETE';
   const watermark = `SIDOMO DCS --- Diunduh oleh: ${req.body.name} (${req.body.nik}) pada ${moment().format('DD/MM/YYYY, HH:mm:ss Z')} --- Status dokumen ${status} pada saat diunduh`;
   let ext = path.parse(filename).ext;
@@ -22,58 +21,61 @@ router.post('/watermark/:filename', (req, res) => {
     res.end(`File is not a PDF. Please convert it first.`);
   }
 
-  const inputPath = path.resolve(__dirname, '../uploads', filename);
+  // This should be a Uint8Array or ArrayBuffer
+  // This data can be obtained in a number of different ways
+  // If your running in a Node environment, you could use fs.readFile()
+  // In the browser, you could make a fetch() call and use res.arrayBuffer()
+  const existingPdfBytes = fs.readFileSync(path.resolve(__dirname, '../uploads', filename));
+
   const outputPath = path.resolve(
     __dirname,
     '../uploads',
     `${filename.slice(0, -4)}_watermarked.pdf`,
   );
 
-  const main = async () => {
-    const pdfdoc = await PDFNet.PDFDoc.createFromFilePath(inputPath);
-    await pdfdoc.initSecurityHandler();
+  // Load a PDFDocument from the existing PDF bytes
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
 
-    const stamper = await PDFNet.Stamper.create(
-      PDFNet.Stamper.SizeType.e_relative_scale,
-      0.5,
-      0.5,
-    ); // Stamp size is relative to the size of the crop box of the destination page
-    stamper.setAlignment(
-      PDFNet.Stamper.HorizontalAlignment.e_horizontal_right,
-      PDFNet.Stamper.VerticalAlignment.e_vertical_center,
-    );
-    const redColorPt = await PDFNet.ColorPt.init(1, 0, 0);
-    stamper.setFontColor(redColorPt);
-    const pgSet = await PDFNet.PageSet.createRange(
-      1,
-      await pdfdoc.getPageCount(),
-    );
-    stamper.setRotation(270);
-    stamper.setPosition(10, 0, false)
-    stamper.setSize(PDFNet.Stamper.SizeType.e_font_size, 12, 0)
-    await stamper.stampText(pdfdoc, watermark, pgSet);
+  // Embed the Helvetica font
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
 
-    await pdfdoc.save(outputPath, PDFNet.SDFDoc.SaveOptions.e_linearized);
-  };
+  // Get the first page of the document
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
 
-  PDFNet.runWithCleanup(main) // you can add the key to PDFNet.runWithCleanup(main, process.env.PDFTRONKEY)
-    .then(() => {
-      PDFNet.shutdown();
-      fs.readFile(outputPath, (err, data) => {
-        if (err) {
-          res.statusCode = 500;
-          res.send(err);
-        } else {
-          res.setHeader('Content-type', 'application/pdf');
-          res.send(data);
-        }
-      });
+  // Get the width and height of the first page
+  const { width, height } = firstPage.getSize()
+
+  // Draw watermark across the ALL pages
+  pages.forEach(function (page) {
+    page.drawText(watermark, {
+      x: width - 10,
+      y: (height - (watermark.length * 5.6)) / 2,
+      size: 11,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(90),
     })
-    .catch(err => {
-      PDFNet.shutdown();
-      // res.statusCode = 500;
+  });
+
+  // Serialize the PDFDocument to bytes (a Uint8Array)
+  const pdfBytes = await pdfDoc.save()
+
+  try {
+    fs.writeFileSync(outputPath, pdfBytes);
+    console.log("File written successfully");
+  } catch (err) {
+    console.error(err);
+  }
+  fs.readFile(outputPath, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
       res.send(err);
-    });
+    } else {
+      res.setHeader('Content-type', 'application/pdf');
+      res.send(data);
+    }
+  });
 });
 
 module.exports = router
